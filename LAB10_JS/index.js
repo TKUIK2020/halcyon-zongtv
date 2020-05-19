8b78596c6955c6cd26b21930838c977b8d4c18b8
zongtv <62640301+zongtv@users.noreply.github.com>
2020-05-19 13:51:15 +0300

    Add files via upload
    LAB10_JS

---
+++ LAB10_JS/index.js
@@ -0,0 +1,28 @@
+//все работает, кроме кнопки с картинкой :(
+//если отдельно запускать скрипт с картинкой, работает...
+const http = require('http');
+const html = `
+<!doctype>
+<html>	
+    <head>
+    <meta charset="utf-8">	
+    <title>Тивончик тест Node.js</title>
+    
+    </head>
+    <body>
+        <h1>Привет мир!</h1>
+        <button><a href="pic.js">picture</button> 
+      
+    </body>
+</html>`;
+server = http.createServer((req,res) => {
+    switch (req.url){
+        case '/':
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end(html);
+        default:
+            res.writeHead(404, {'Content-Type': 'text/plain'});
+	        res.end("404 error");
+    }
+	
+}).listen(3000, () => console.log('server ON'));
