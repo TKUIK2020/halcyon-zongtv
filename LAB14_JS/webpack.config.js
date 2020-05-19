fde8435f66cad9bd4fddabbcfeefa26d2172fe56
zongtv <62640301+zongtv@users.noreply.github.com>
2020-05-19 13:44:02 +0300

    Add files via upload
    LAB14_JS

---
+++ LAB14_JS/webpack.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+    mode: 'development',
+    entry: "./Lab14/there.js",
+    output: {
+        filename: "bundle.js"
+    },
+  
+
+//вроде работает ?\_(?)_/?  
+    module: {
+      rules: [
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread']
+            }
+          }
+        }
+      ]
+    }
+  }
